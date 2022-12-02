c15e486f9865a592f4597f5223d0b8bea97b70b8
Cao Mingjun <me@caomingjun.com>
2022-12-02 11:55:00 +0800

    add multiprocess test

--- batch_test.py
+++ batch_test.py
@@ -4,10 +4,13 @@ from pdr import pdr
 from dataloader import TestCase
 import pandas as pd
 from tqdm import tqdm
-import traceback
+import multiprocessing as mp
+import sys
+from itertools import product
 
-DATA_DIR_ROOT = "../Dataset-of-Pedestrian-Dead-Reckoning/"
+DATA_DIR_ROOT = "Dataset-of-Pedestrian-Dead-Reckoning/"
 WALK=["Hand-Walk", "Bag-Walk", "Pocket-Walk"]
+EXCLUDE=["Hand-Walk/Hand-Walk-05-001", "Hand-Walk/Hand-Walk-05-002"]
 
 def main():
     parser = argparse.ArgumentParser()
@@ -26,7 +29,8 @@ def main():
     else:
         for dir in os.listdir(DATA_DIR_ROOT):
             datasets += [os.path.join(dir, name) for name in os.listdir(os.path.join(DATA_DIR_ROOT, dir))]
-
+    datasets = [dataset for dataset in datasets if dataset not in EXCLUDE]
+    
     names=[]
     dist_error=[]
     dir_error=[]
@@ -41,10 +45,11 @@ def main():
             dir_ratio.append(test_case.get_dir_ratio())
             names.append(dataset)
         except Exception as e:
-            # print(f"Error when processing dataset {dataset}:")
+            print(f"Error when processing dataset {dataset}: {e}")
             # traceback.print_exc()
             fails+=1
             continue
+    df = pd.DataFrame({"name": names, "dist_error": dist_error, "dir_error": dir_error, "dir_ratio": dir_ratio})
     df = pd.DataFrame({"dataset": names, "dist_error": dist_error, "dir_error": dir_error, "dir_ratio": dir_ratio})
     df.to_csv("result.csv", index=False)
     print("Done, results saved to result.csv")
@@ -83,7 +88,96 @@ def test_step_method():
         success.append(len(_dist_error))
     df = pd.DataFrame({"method": METHODS, "dist_error": dist_error, "dir_error": dir_error, "dir_ratio": dir_ratio, "success": success})
     df.to_csv("result_step_method.csv", index=False)
+
+def worker(_args):
+    try:
+        test_case = TestCase(os.path.join(DATA_DIR_ROOT, _args["dataset"]))
+        pdr(
+            test_case, 
+            model_name=_args["model_name"],
+            distance_frac_step=_args["distance_frac_step"],
+            clean_data=_args["clean_data"], 
+            optimized_mode_ratio=_args["optimized_mode_ratio"],
+            butter_Wn=_args["butter_Wn"])
+        dist_error = test_case.get_dist_error()
+        dir_error = test_case.get_dir_error()
+        dir_ratio = test_case.get_dir_ratio()
+        return _args["dataset"], _args["model_name"], _args["distance_frac_step"], _args["clean_data"], _args["optimized_mode_ratio"], _args["butter_Wn"], dist_error, dir_error, dir_ratio
+    except Exception as e:
+        return _args["dataset"], _args["model_name"], _args["distance_frac_step"], _args["clean_data"], _args["optimized_mode_ratio"], _args["butter_Wn"], 0, 0, 0
+
+def mute():
+    sys.stdout = open(os.devnull, 'w')
+    sys.stderr = open(os.devnull, 'w')
+
+
+def mp_test():
+    datasets = []
+    for dir in WALK:
+        datasets += [os.path.join(dir, name) for name in os.listdir(os.path.join(DATA_DIR_ROOT, dir))]
+    datasets = [dataset for dataset in datasets if dataset not in EXCLUDE]
+    args = {
+        "model_name": {
+            "default":"RandomForest",
+            "options": ["SVR", "RandomForest", "Mean", "AdaBoost", "ExtraTree"]
+        },
+        "distance_frac_step": {
+            "default": 5,
+            "options": [3,4,5,6,7]
+        },
+        "clean_data": {
+            "default": 5,
+            "options": [3,4,5,6,7,8,9,10]
+        },
+        "optimized_mode_ratio": {
+            "default": 0.9,
+            "options": [0, 0.1, 0.2, 0.4, 0.5, 0.6, 0.8, 0.9, 1.0]
+        },
+        "butter_Wn": {
+            "default": 0.005,
+            "options": [0.001, 0.002, 0.005, 0.01, 0.02, 0.03, 0.05, 0.08, 0.1]
+        },
+    }
+
+
+    datasets.append("../test_case0")
+    todos=[]
+    # for dataset in datasets:
+    #     for key, value in args.items():
+    #         for option in value["options"]:
+    #             _args = {
+    #                 "dataset": dataset,
+    #                 "model_name": args["model_name"]["default"],
+    #                 "distance_frac_step": args["distance_frac_step"]["default"],
+    #                 "clean_data": args["clean_data"]["default"],
+    #                 "optimized_mode_ratio": args["optimized_mode_ratio"]["default"],
+    #                 "butter_Wn": args["butter_Wn"]["default"]
+    #             }
+    #             _args[key] = option
+    #             todos.append(_args)
+
+    for todo in product(datasets,
+        args["model_name"]["options"], 
+        args["distance_frac_step"]["options"],
+        args["clean_data"]["options"],
+        args["optimized_mode_ratio"]["options"],
+        args["butter_Wn"]["options"]):
+        todos.append({
+            "dataset": todo[0],
+            "model_name": todo[1],
+            "distance_frac_step": todo[2],
+            "clean_data": todo[3],
+            "optimized_mode_ratio": todo[4],
+            "butter_Wn": todo[5]
+        })
+    
+
+
+    with mp.Pool(processes=72, initializer=mute) as pool:
+        res = list(tqdm(pool.imap(worker, todos), total=len(todos)))
+    df = pd.DataFrame(res, columns=["dataset", "model_name", "distance_frac_step", "clean_data", "optimized_mode_ratio", "butter_Wn", "dist_error", "dir_error", "dir_ratio"])
+    df.to_csv("result_mp_adjust_case0.csv", index=False)
         
 
 if __name__ == "__main__":
-    main()
+    mp_test()
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ parser.add_argument("--silent", action="store_true", help="whether to print dist
 
 args = parser.parse_args()
 test_case = TestCase(args.dataset)
-pdr(test_case)
+pdr(test_case,model_name="Adaboost")
 if not args.silent:
     if test_case.have_location_valid:
         dist_error = test_case.get_dist_error()
