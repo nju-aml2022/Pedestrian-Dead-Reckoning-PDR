4685460cab48db47464ff8362763116b824df8c7
Cao Mingjun <me@caomingjun.com>
2022-12-02 17:41:51 +0800

    display time used in main.py

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import argparse
 from pdr import pdr
 from dataloader import TestCase
+import time
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", type=str, help="dataset location", required=True)
 parser.add_argument("--silent", action="store_true", help="whether to print distance and direction error")
 
 args = parser.parse_args()
+start=time.time()
 test_case = TestCase(args.dataset)
 pdr(test_case)
 if not args.silent:
+    print(f"Done in {time.time()-start} seconds")
     if test_case.have_location_valid:
         dist_error = test_case.get_dist_error()
         dir_error = test_case.get_dir_error()
